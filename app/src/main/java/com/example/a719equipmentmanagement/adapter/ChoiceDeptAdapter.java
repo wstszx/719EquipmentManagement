0a7f3c8875a6d3e1062bd63ca7234c86a2c251ee
yangchi <18871430038@163.com>
2019-05-21 15:38:07 +0800

    修改错误处理

---
+++ app/src/main/java/com/example/a719equipmentmanagement/adapter/ChoiceDeptAdapter.java
@@ -0,0 +1,16 @@
+package com.example.a719equipmentmanagement.adapter;
+
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.BaseViewHolder;
+import com.example.a719equipmentmanagement.entity.TreeData;
+
+public class ChoiceDeptAdapter extends BaseQuickAdapter<TreeData, BaseViewHolder> {
+    public ChoiceDeptAdapter(int layoutResId) {
+        super(layoutResId);
+    }
+
+    @Override
+    protected void convert(BaseViewHolder helper, TreeData item) {
+
+    }
+}
