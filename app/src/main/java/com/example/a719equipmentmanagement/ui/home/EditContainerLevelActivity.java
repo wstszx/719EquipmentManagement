6892eda4c4f5d7708a99dad8432f78f7b2f928a8
yangchi <18871430038@163.com>
2019-06-06 23:50:08 +0800

    修改ui

--- app/src/main/java/com/example/a719equipmentmanagement/ui/home/EditContainerLevelActivity.java
+++ app/src/main/java/com/example/a719equipmentmanagement/ui/home/EditContainerLevelActivity.java
@@ -1,26 +1,32 @@
 package com.example.a719equipmentmanagement.ui.home;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.a719equipmentmanagement.R;
 import com.example.a719equipmentmanagement.base.BaseActivity;
 import com.qmuiteam.qmui.widget.QMUITopBarLayout;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class EditContainerLevelActivity extends BaseActivity {
 
     @BindView(R.id.topbar)
     QMUITopBarLayout topbar;
+    @BindView(R.id.edittext)
+    EditText edittext;
+    @BindView(R.id.tv_result1)
+    TextView tvResult1;
+
     @Override
     protected void init(Bundle savedInstanceState) {
         initTopbar();
     }
 
     private void initTopbar() {
-        topbar.setTitle("编辑货柜层数");
+        topbar.setTitle("编辑货柜层");
         topbar.addRightTextButton(R.string.confirm, R.id.confirm).setOnClickListener(v -> {
             topbar.removeAllRightViews();
 //            editContainer();
@@ -30,8 +36,10 @@ public class EditContainerLevelActivity extends BaseActivity {
             overridePendingTransition(R.anim.slide_still, R.anim.slide_out_right);
         });
     }
+
     @Override
     protected int getLayoutId() {
         return R.layout.activity_edit_container_level;
     }
+
 }
